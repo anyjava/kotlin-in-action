303458d0c64743d5d561554ccb2114ca95a51667
anyjava <sht21c@gmail.com>
2019-10-10 09:06:55 +0900

    init commit

---
+++ src/dev/anyjava/kotlin/FirstKotlinApp.kt
@@ -0,0 +1,9 @@
+package dev.anyjava.kotlin
+
+data class Person(val name: String, val age: Int? = null)
+
+fun main(args: Array<String>) {
+    val persons = listOf(Person("영희"), Person("철수", age = 29))
+    val oldest = persons.maxBy{ it.age ?: 0}
+    println("oldest: $oldest")
+}
