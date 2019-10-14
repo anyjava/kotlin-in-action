d997ccd5c6b5d8be52cfc8c0fcffbcf8a380d322
anyjava <sht21c@gmail.com>
2019-10-14 09:20:58 +0900

    ADD operator override sample.

--- src/dev/anyjava/kotlin/FirstKotlinApp.kt
+++ src/dev/anyjava/kotlin/FirstKotlinApp.kt
@@ -1,9 +1,26 @@
 package dev.anyjava.kotlin
 
-data class Person(val name: String, val age: Int? = null)
+data class Person(var name: String, var age: Int? = null)
+
+operator fun Person.plus(p : Person) : Person {
+    return Person("plus operator", this.age!! + p.age!!)
+}
+
+operator fun Person.plusAssign(p : Person) : Unit {
+    this.name = "plusAssign"
+    this.age = this.age?.plus(p.age!!)
+}
 
 fun main(args: Array<String>) {
     val persons = listOf(Person("영희"), Person("철수", age = 29))
     val oldest = persons.maxBy{ it.age ?: 0}
     println("oldest: $oldest")
+
+    // plus & plusAssign operator override
+    val p1 = Person("A", 1)
+    val p2 = Person("B", 2)
+
+    p1 += p2
+    println(p1)
 }
+
